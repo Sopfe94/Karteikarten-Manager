c5a02723a991cf4cb43978dc5a6febd0d9a04219
sopfe94 <info.gross-apps@gmx.de>
2026-05-20 19:29:12 +0200

    update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,6 @@
 // Service Worker v10 - 24h Cache für index.html (Beta: kein Cache)
 const CACHE = ‘kkm-v10’;
+const BASE = self.location.hostname === 'www.gross-apps.de' ? '/karteikarten' : '';
 const CACHE_TS_KEY = ‘kkm-html-ts’;
 const IS_BETA = /beta|localhost/.test(self.location.hostname);
 const ONE_DAY = IS_BETA ? 0 : 24 * 60 * 60 * 1000;
